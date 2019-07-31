9309649f7864f86f4438ec4fbda8e23f0fa6b549
Rafael Walter <rafawalter@gmail.com>
2019-07-31 10:50:52 -0300

    Data Binding in Angular Forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   languages = ['English', 'Spanish', 'Other'];
+  model = new Employee('Darla', 'Smith', true, 'W2', 'English');
+
+  firstNameToUpperCase(value: string) {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
+    }
+  }
 }
